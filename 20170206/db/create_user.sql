07dcee090f1cc8f45d9303492926e9badc73e0e9
weerci <weerci@yandex.ru>
2017-02-07 01:25:50 +0700

    001

--- 20170206/db/create_user.sql
+++ 20170206/db/create_user.sql
@@ -1,18 +1,17 @@
-/*Создаем пользователей под коннектом system*/
+/*Создаем пользователей под коннектом sysdba*/
 drop user modern cascade;
 create user modern identified by modern
 default tablespace USERS temporary tablespace TEMP profile DEFAULT;
 grant resource, dba, connect to modern;
 
 drop user auth cascade;
-create user auth identified by auth
+create user auth identified by Ijf34nfD
 default tablespace USERS temporary tablespace TEMP profile DEFAULT;
 grant resource, dba, connect to auth;
 
 /*Импортируем данные из файла дампа*/
 imp system/sys@XE file="C:\temp\dna.dmp" fromuser="modern, auth" touser="modern, auth"
 
-/*Права раздаем под коннектом sysdba*/
 grant execute on dbms_crypto to modern;
 grant execute on modern.logon to auth;
 grant select on sys.DBA_CONSTRAINTS to modern;
